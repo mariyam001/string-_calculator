aaf21f6153a076eb8885b7b11062c56eedf3dd97
mariyam001 <mariyamkauser2@gmail.com>
2024-04-25 02:31:03 +0530

    added all cases of string calculation

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,23 @@ class StringCalculator
 
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(',').map(&:to_i).sum
+
+    if numbers.start_with?("//")
+      delimiter_index = numbers.index("\n")
+      delimiter = numbers[2..delimiter_index - 1]
+      numbers = numbers[delimiter_index + 1..-1]
+    else
+      delimiter = ","
+      numbers = numbers
+    end
+
+    numbers_array = numbers.split(/#{delimiter}|\n/)
+    numbers_array.map!(&:to_i)
+    negative_numbers = numbers_array.select { |num| num < 0 }
+    if negative_numbers.any?
+      raise StandardError, "negative numbers not allowed #{negative_numbers.join(',')}"
+    end
+    sum = numbers_array.sum
+
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -11,12 +11,61 @@ describe StringCalculator do
       expect(@string_calculator.add("")).to eq(0)
     end
 
-    it 'returns the number itself for a single number' do
-      expect(@string_calculator.add("1")).to eq(1)
+    describe 'when there is one argument' do
+      it 'must return this argument' do
+        expect(@string_calculator.add("1")).to eq(1)
+      end
     end
 
-    it 'returns the sum of two numbers' do
-      expect(@string_calculator.add("1,2")).to eq(3)
+    describe 'multiple numbers' do
+      describe 'when there are two numbers' do
+        it 'must add the numbers' do
+          expect(@string_calculator.add("1,2")).to eq(3)
+        end
+      end
+
+      describe 'when the numbers are separated by commas' do
+        it 'must add all the numbers' do
+          expect(@string_calculator.add("1,2,3,4")).to eq(10)
+        end
+      end
+
+      describe 'when there are newlines between the numbers' do
+        it 'handles new lines between number' do
+          expect(@string_calculator.add("1\n2,3")).to eq(6)
+        end
+      end
     end
+
+    describe 'when there is a delimiter defined' do
+      it 'must use the new delimiter' do
+        expect(@string_calculator.add("//;\n1;2")).to eq(3)
+      end
+
+      it 'must use the ! delimiter' do
+        expect(@string_calculator.add("//!\n1!2")).to eq(3)
+      end
+
+      describe 'with delimiters of longer than 1 char' do
+        it 'can parse the delimiter correctly' do
+          expect(@string_calculator.add("//[***]\n1***2***3")).to eq(6)
+        end
+
+        it 'can parse the delimiter correctly if the delimiter consists of different chars' do
+          expect(@string_calculator.add("//[**!]\n1**!2**!3")).to eq(6)
+        end
+      end
+    end
+
+    describe 'when there are negative numbers' do
+      it 'raises an error and shows the negative number' do
+       expect { @string_calculator.add("1,2,3,-4") }.to raise_error(StandardError, "negative numbers not allowed -4")
+     end
+
+     it 'raises an error and shows multiple negative numbers' do
+       expect { @string_calculator.add("1,-2,3,-4") }.to raise_error(StandardError, "negative numbers not allowed -2,-4")
+     end
+   end
+
   end
 end
